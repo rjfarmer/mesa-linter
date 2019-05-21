154095d54203be960aa6e0480012df50c92e238c
Robert Farmer <r.j.farmer@uva.nl>
2019-05-21 10:11:58 -0700

    More fixes

--- mesa-linter.py
+++ mesa-linter.py
@@ -28,10 +28,10 @@ def check_float(line):
     
 def check_crlibm(line):
     # Match the function with the bracket as crlibm puts a _cr at the end of each function
-    checks = ['log(','exp(','cos(','sin(','log10(','exp10(','tan(']
+    checks = ['log(','exp(','cos(','sin(','log10(','exp10(','tan(','dlog(']
     found = []
     for c in checks:
-        if c in line:
+        if c in line and not '_'+c in line:
             found.append(c.replace('(',''))
     if len(found):
         return "Not using crlibm function",found
@@ -39,8 +39,8 @@ def check_crlibm(line):
     
 def check_pow(line):
     # Look for 3**5
-    if re.search("[0-9]\*\*[0-9]", line):
-        return "Maybe found ** use, use powX() instead"
+    if re.search("[a-zA-Z0-9\)]\*\*[a-zA-Z0-9]", line):
+        return "Found ** use, use powX() instead"
     return None
     
 def check_real_op(line):
@@ -56,15 +56,17 @@ def check_real_op(line):
     
 def check_real_exp(line):
     # Look for 1e+1, 1e-1, 1e1
-    if re.search("[0-9]e[+?][-?][0-9]", line):
+    if re.search("[0-9][eE][+]?[-]?[0-9]", line):
         return "Found use of exponent E, use D instead"
     return None
     
+# Abit broken at the moment
 def check_real_d(line):
-    # Look for 1.5 but not 1.5d0
-    if (re.search("([0-9]\.[0-9]+)(?!d)", line) and re.search("([0-9]\.[0-9]+)(?!_)", line)) or (re.search("([0-9]\.\s)", line)):
-        return "Missing D on float"
     return None
+    # # Look for 1.5 but not 1.5d0
+    # if re.search("([0-9]\.[0-9]*)(?!(d|D|_)\b)", line):
+        # return "Missing D on float"
+    # return None
     
 def check_real(line):
     # Look for declaring things real and not real(dp)
